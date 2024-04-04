08e3bd5b8cb26fac01672ff994023edfa967da23
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-04 17:54:53 +0800

    Add http trigger function.

---
+++ quickstarts/HelloFunctions/HttpTriggerFunction.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace HelloFunctions
+{
+    public class HttpTriggerFunction
+    {
+        private readonly ILogger<HttpTriggerFunction> _logger;
+
+        public HttpTriggerFunction(ILogger<HttpTriggerFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HttpTriggerFunction")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
